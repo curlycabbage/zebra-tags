66102701f78e0382b497831e6eb54a4adecb07fb
Peter Radocchia <radocchia@gmail.com>
2021-06-05 09:53:44 -0400

    skipped a stitch

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as ShelfTag200x125 } from "./ShelfTag200x125.vue";
 export { default as SaleTag325x450 } from "./SaleTag325x450.vue";
+export { default as ItemTag325x450 } from "./ItemTag325x450.vue";
 export { sendToPrinter, sendToProxy } from "./utils";
